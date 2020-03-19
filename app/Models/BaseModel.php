a6594a803aa3a91c7a284773e533a20bb09cd99a
Selim Gormus (dell) <info@siberfx.com>
2020-03-19 18:25:51 +0300

    phpdoc updated

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,8 +2,18 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\BaseModel
+ *
+ * @method static Builder|BaseModel newModelQuery()
+ * @method static Builder|BaseModel newQuery()
+ * @method static Builder|BaseModel query()
+ * @mixin Eloquent
+ */
 class BaseModel extends Model
 {
     const STATUS_ACTIVE = 'active';
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
